f76fa5ba033aa94aab4b1343e2a56cab8488ad10
Catto <catto@live.com>
2024-04-03 09:48:06 -0400

    GA4 alter attempt to get working

--- app/components/Utils/GA4/GA4.tsx
+++ app/components/Utils/GA4/GA4.tsx
@@ -15,7 +15,14 @@ const GA4TagCatto = () => {
         <script>
             window.dataLayer = window.dataLayer || [];
         </script>
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-1PMYR3ETW4');
+        </script>
+        {/* <Script id="google-tag-manager" strategy="afterInteractive">
             {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -23,7 +30,7 @@ const GA4TagCatto = () => {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
             `}
-        </Script>
+        </Script> */}
       </>
     )
   }
