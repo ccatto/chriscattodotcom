3804d39ff49c3a452fa2b52f6aa98ada4eabd2f3
Catto <catto@live.com>
2024-04-07 20:22:02 -0400

    git page structure.

--- app/code/git/page.tsx
+++ app/code/git/page.tsx
@@ -1,6 +1,6 @@
-// import JumbotronCattoCode from "../components/JumbotronCattoCode/JumbotronCattoCode"
 import { Metadata } from 'next'
 import JumbotronCattoFlexible from "@/app/components/JumbotronCattoFlexible/JumbotronCattoFlexible"
+import SideNavGitCatto from '@/app/components/SideNavGitCatto/SideNavGitCatto'
 
 export const metadata: Metadata = {
     title: 'Chris Catto Code Git Source Control',
@@ -10,61 +10,18 @@ export const metadata: Metadata = {
 export default function Page() {
     return (
         <>
+            <div className="flex">
+                <aside className="flex-[20] bg-red-600">
+                    <SideNavGitCatto />
+                </aside>
+                <article className="flex-[80] bg-blue-500">
+                    <JumbotronCattoFlexible
+                        title="Git Source Control"
+                        description="Git is the industry standard for source control. This section I am displaying some of the commands that I use frequently."
+                    />
+                </article>
+            </div>
             <hr className="m-3" />
-            <JumbotronCattoFlexible
-                title="Git Source Control"
-                description="Git is the industry standard for source control. This section I am displaying some of the commands that I use frequently."
-            />
-            <hr className="m-3" />
-            <section>
-                This is some raw ideas of how to organize the Git section:
-                ------------
-                https://www.datacamp.com/tutorial/github-and-git-tutorial-for-beginners
-                Definition:
-                What is git
-                github vs git vs gitlab
-                How does it work
-                Branches
-                commits
-
-                ------------
-                * Installing Git
-                ------------
-                * Commands:
-                * Git Version
-                * Git
-                init
-                clone
-                add
-                status
-                push
-                checkout -b
-                remote -v
-                branch
-                pull
-                merge
-                log
-                rebase
-                clean
-                git rebase
-
-                ------------
-                * Tools:
-                * SourceTree
-                * Github desktop
-                ------------
-                * Tips:
-                * Create Shell function to add commit & push all in one command easily;
-
-                create a shell function:
-                {/* 
-                    function qg() {
-                        git add .
-                        git commit -a -m "$*"
-                        git push 
-                    }
-                */}
-            </section>
         </>
     )
 }
---
+++ app/components/SideNavGitCatto/SideNavGitCatto.tsx
@@ -0,0 +1,15 @@
+const SideNavGitCatto = () => {
+  return (
+    <>
+        <div className="flex">
+          <aside className="flex-1  bg-red-600">
+            Aside
+          </aside>
+          <article className="flex-1 bg-blue-500">
+            Main
+          </article>
+        </div>
+    </>
+  )
+}
+export default SideNavGitCatto;
