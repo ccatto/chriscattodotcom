077a493f9b47967b6c5442eb1b8cf7ed33a80652
Catto <catto@live.com>
2024-04-23 05:45:28 -0400

    updates nav git js.

--- app/(public)/code/javascript/fundamentals/helloworld/page.tsx
+++ app/(public)/code/javascript/fundamentals/helloworld/page.tsx
@@ -14,14 +14,14 @@ const fundamentals = () => {
   return (
     <>
       <div className="flex flex-col flex-nowrap h-screen w-full ">
-        <div className='flex-[20]'>
+        <div className='flex-none'>
           <JumbotronCattoFlexible
             title="JavaScript Fundamentals Hello World"
             description="JavaScript fundamentals including a hello world example; variables data types; operations; functions ect. "
           />
         </div>
         <hr className="p-0" />
-        <div className='flex-[80] m-4 p-4 rounded-2xl dark:bg-[#f7df1e] text-black ring-2 ring-gray-300 dark:ring-gray-500'>
+        <div className='flex-1 m-4 p-4 rounded-2xl dark:bg-[#f7df1e] text-black ring-2 ring-gray-300 dark:ring-gray-500'>
           <h2 className="inline-block mb-4 text-3xl font-extrabold tracking-tight">
             JavaScript Fundamentals Hello World
           </h2>
