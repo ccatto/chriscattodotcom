f51f5e952b22733d646c7517e6dae7cdece893a3
Catto <catto@live.com>
2024-05-01 17:27:26 -0400

    contact

--- app/components/Forms/ContactFormCatto.tsx
+++ app/components/Forms/ContactFormCatto.tsx
@@ -48,7 +48,6 @@ const ContactFormCatto = () => {
 
   useEffect(() => {
     if (isSubmitSuccessful) {
-      console.log('------- YO Its successful');
       setIsSubmitSuccessfulTrue(true);
     }
   }, [isSubmitSuccessful]);
@@ -88,7 +87,6 @@ const ContactFormCatto = () => {
                       type="text"
                       placeholder="Email"
                       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                      // className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       autoFocus
                     />
                   </div>
