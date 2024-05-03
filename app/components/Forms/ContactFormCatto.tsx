d6de987a8a1a2eae34a52719ab10a77f31414d4f
Catto <catto@live.com>
2024-05-03 15:24:56 -0400

    resolve the mail again button.

--- app/components/Forms/ContactFormCatto.tsx
+++ app/components/Forms/ContactFormCatto.tsx
@@ -47,6 +47,7 @@ const ContactFormCatto = () => {
       if (response.status === 200) {
         // console.log('in success response === ', response);
         setIsSubmitSuccessfulTrue(true);
+        // resizeTo({});
       }
     } catch {
       // console.log('YO an ERROR not 200');
@@ -56,6 +57,12 @@ const ContactFormCatto = () => {
       setIsEmailSentFail500(true);
     }
   };
+
+  const handleMailAgainClick = () => {
+    console.log("inside handleMailAgainClick()");
+    setIsSubmitSuccessfulTrue(false);
+    // reset();
+  }
 
   return (
     <>
@@ -179,8 +186,8 @@ const ContactFormCatto = () => {
             </div>
             <hr className="m-5 mx-auto my-4 h-1 w-48 rounded border-0 bg-gray-100 dark:bg-gray-200 md:my-10" />
             <div className="mb-5 flex items-center justify-center">
-              <Link
-                href="/contact"
+              <button
+                onClick={handleMailAgainClick}
                 className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
               >
                 Send another message
@@ -199,7 +206,7 @@ const ContactFormCatto = () => {
                     d="M1 5h12m0 0L9 1m4 4L9 9"
                   />
                 </svg>
-              </Link>
+              </button>
             </div>
           </div>
         </div>
@@ -238,11 +245,11 @@ const ContactFormCatto = () => {
           </div>
           <hr className="m-5 mx-auto my-4 h-1 w-48 rounded border-0 bg-gray-100 dark:bg-gray-200 md:my-10" />
           <div className="mb-5 flex items-center justify-center">
-            <Link
-              href="/contact"
+            <button
+              onClick={handleMailAgainClick}
               className="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
             >
-              Try the email again
+              Send another email
               <svg
                 className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
                 aria-hidden="true"
@@ -258,7 +265,7 @@ const ContactFormCatto = () => {
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg>
-            </Link>
+            </button>
           </div>
         </div>
       )}
