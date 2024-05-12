3e57b99e963e64d79778fbf99b1587df1043a14e
Catto <catto@live.com>
2024-05-12 16:13:12 -0400

    updated stripe doc page to remove the error.

---
+++ app/(public)/code/payment-processors/stripe/page.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from 'next';
+import JumbotronCattoFlexible from '@/app/components/JumbotronCattoFlexible/JumbotronCattoFlexible';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Chris Catto Code Database PostgreSQL',
+  description: 'Chris Catto Code Database PostgreSQL',
+};
+
+export default function Page() {
+  return (
+    <>
+      <div className="flex h-full w-full flex-col flex-nowrap ">
+        <div className="flex-1">
+          <JumbotronCattoFlexible
+            title="Stripe Payment Processing"
+            description="Stripe is a payment processor that I enjoy using. This is an outline of what type of development effort it takes."
+          />
+        </div>
+        <div className="dark:text-slate-800 m-4 flex-[80] rounded-2xl p-4 ring-2 ring-gray-300 dark:bg-[#5433FF]">
+          <p className="first-letter:float-start mb-3 text-gray-800 first-letter:me-3 first-letter:text-5xl first-letter:font-bold first-letter:text-slate-900 first-line:uppercase first-line:tracking-widest dark:text-gray-800 dark:first-letter:text-gray-800">
+            Stripe is payment processor where we can setup payments for any business & get started quickly. It is flexible & PCI compliant. It allows us to host our own payment page & create shareable links. This is obviously a starter page & is a work in progress.
+          </p>
+        </div>
+        <hr className="m-3" />
+      </div>
+    </>
+  );
+}
