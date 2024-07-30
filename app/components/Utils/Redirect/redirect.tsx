2933bdaf3f5afa6584c25f1e30908bd696c91da3
Catto <catto@live.com>
2024-07-30 10:40:02 -0400

    css-styles prettier

--- app/components/Utils/Redirect/redirect.tsx
+++ app/components/Utils/Redirect/redirect.tsx
@@ -1,6 +1,6 @@
-"use server";
+'use server';
 
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 
 export async function navigate(path: string) {
   redirect(path);
