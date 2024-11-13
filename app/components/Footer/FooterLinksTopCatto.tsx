f60cd27a7181a48f3813769056382ea0fa51f1ac
Catto <catto@live.com>
2024-11-13 06:24:50 -0500

    adding .net section

--- app/components/Footer/FooterLinksTopCatto.tsx
+++ app/components/Footer/FooterLinksTopCatto.tsx
@@ -67,7 +67,7 @@ const FooterLinksTopCatto = () => {
               <Accordion type="single" collapsible defaultValue="item-1">
                 <AccordionItem value="item-1">
                   <AccordionTrigger>
-                    <span className="pl-2">JavaScript Topics</span>
+                    <span className="pl-2">JavaScript Topics2</span>
                   </AccordionTrigger>
                   <AccordionContent>
                     <ul>
@@ -131,6 +131,73 @@ const FooterLinksTopCatto = () => {
                           className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                         >
                           <span className="ms-3">Remove item from array</span>
+                        </Link>
+                      </li>
+                    </ul>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            </li>
+            <li className="mb-4">
+              <Accordion type="single" collapsible defaultValue="item-1">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>
+                    <span className="pl-2">MS .NET</span>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <ul>
+                      {/* .NET Overview */}
+                      <li className="-scroll-mt-3">
+                        <Link
+                          href="/code/.NET/overview"
+                          className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                        >
+                          <svg
+                            className="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 18 21"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M6,9 C6.55228,9 7,9.44772 7,10 L7,14 C7,14.5523 6.55228,15 6,15 L2,15 C1.44772,15 1,14.5523 1,14 L1,10 C1,9.44772 1.44772,9 2,9 L6,9 Z M14,13 C14.5523,13 15,13.4477 15,14 C15,14.5523 14.5523,15 14,15 L10,15 C9.44772,15 9,14.5523 9,14 C9,13.4477 9.44772,13 10,13 L14,13 Z M5,11 L3,11 L3,13 L5,13 L5,11 Z M14,9 C14.5523,9 15,9.44772 15,10 C15,10.5523 14.5523,11 14,11 L10,11 C9.44772,11 9,10.5523 9,10 C9,9.44772 9.44772,9 10,9 L14,9 Z M6,1 C6.55228,1 7,1.44772 7,2 L7,6 C7,6.55228 6.55228,7 6,7 L2,7 C1.44772,7 1,6.55228 1,6 L1,2 C1,1.44772 1.44772,1 2,1 L6,1 Z M14,5 C14.5523,5 15,5.44772 15,6 C15,6.51283143 14.613973,6.93550653 14.1166239,6.9932722 L14,7 L10,7 C9.44772,7 9,6.55228 9,6 C9,5.48716857 9.38604429,5.06449347 9.88337975,5.0067278 L10,5 L14,5 Z M5,3 L3,3 L3,5 L5,5 L5,3 Z M14,1 C14.5523,1 15,1.44772 15,2 C15,2.55228 14.5523,3 14,3 L10,3 C9.44772,3 9,2.55228 9,2 C9,1.44772 9.44772,1 10,1 L14,1 Z"
+                            />
+                          </svg>
+                          <span className="ms-3">.NET Overview</span>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="/code/.NET/v9"
+                          className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                        >
+                          <svg
+                            className="h-5 w-5 text-gray-800 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              stroke="#9cafa3"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M12 5.365V3m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175 0 .593 0 1.292-.538 1.292H5.538C5 18 5 17.301 5 16.708c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 12 5.365ZM8.733 18c.094.852.306 1.54.944 2.112a3.48 3.48 0 0 0 4.646 0c.638-.572 1.236-1.26 1.33-2.112h-6.92Z"
+                            />
+                          </svg>
+                          <span className="ms-3">.NET v9</span>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="/code/dotnet/c-sharp/v13"
+                          className="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                        >
+                          <span className="ms-3">C# v13</span>
                         </Link>
                       </li>
                     </ul>
