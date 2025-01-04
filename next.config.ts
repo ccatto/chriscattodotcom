9132061a1eaef41bfc5c6610b126c792acb52694
Catto <catto@live.com>
2025-01-03 19:30:33 -0500

    2025 Chris Catto

---
+++ next.config.ts
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+  output: 'standalone',
+  // env: {
+  //   BASE_URL: process.env.BASE_URL,
+  // }
+
+  // experimental: {
+  //   serverActions: true,
+  // },
+  // images: {
+  // domains: ['images.somedomain.com'],
+  // remotePatters: [
+  //     {
+  //         protocol: 'https',
+  //         hostname: 'images.pexels.com',
+  //         port: '',
+  //         pathname: '/photos/**'
+  //     }
+  // ]
+  // }
+};
+
+module.exports = nextConfig;
